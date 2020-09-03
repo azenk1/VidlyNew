73665b00e6641d772c3f891911cace13215bd314
Alex Zenk <azenk@css.edu>
2020-09-03 10:34:09 -0500

    Init.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,7 +14,46 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { name = "Shrek" };
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer{ Name = "Customer1"},
+             new Customer { Name = "Customer2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content("id=" + movieId);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if(!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if(String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1-12)}")]
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
+        }
+        
     }
 }
